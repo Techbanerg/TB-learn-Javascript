fcb7646f992cd01ee3fc43e5b8ed6c89988a8abe
tech.banerg <tech.banerg@gmail.com>
2019-12-15 15:09:04 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,7 +2,6 @@
 // 1. Hoisting
 // 2. recursive
 // 3. functiona as values
-
 // A function is a group of reusable code which can be called anywhere in your
 // program. This eliminates the need of writing the same code again and again. It
 // helps programmers in writing modular codes. Functions allow a programmer to
